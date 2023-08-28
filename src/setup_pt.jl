b5c743391775c9466b925f30bb6b9528026234ba
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2023-08-28 22:31:39 +0100

    Calculates layer properties based on composition. Can prescribe mixing ratios. Plotting updates.

--- src/setup_pt.jl
+++ src/setup_pt.jl
@@ -8,8 +8,34 @@ end
 
 module setup_pt
 
+    include("phys.jl")
+
+    function isothermal!(atmos, set_tmp)
+        if !atmos.is_param
+            error("Atmosphere parameters not set")
+        end 
+
+        atmos.tmpl[:] .= set_tmp 
+        atmos.tmp[:] .= set_tmp
+
+    end # end isothermal
+
     function dry_adiabat!(atmos)
-        
-    end
+        if !(atmos.is_alloc && atmos.is_param) 
+            error("Atmosphere is not setup")
+        end 
+
+        # Calculate cell-centre values
+        for idx in 1:atmos.nlev_c
+            atmos.tmp[idx] = atmos.tstar * ( atmos.p[idx] / atmos.pl[end] ) ^ ( phys.R_gas / atmos.layer_cp[idx] )
+        end
+
+        # Interpolate to cell-edge values 
+        atmos.tmpl[1]   = atmos.tmp[1]
+        atmos.tmpl[end] = atmos.tmp[end]
+        for idx in 2:atmos.nlev_l-1
+            atmos.tmpl[idx] = 0.5 * (atmos.tmp[idx-1] + atmos.tmp[idx])
+        end 
+    end # end dry_adiabat
      
 end 
