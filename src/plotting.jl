7b5594646c1116a35e7f042dc39381365ce413a9
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2023-08-28 14:08:10 +0100

    Works fine. Makes plots of PT and fluxes.

---
+++ src/plotting.jl
@@ -0,0 +1,69 @@
+# Contains functions for plotting/diagnosing the atmosphere 
+
+# Not for direct execution
+if (abspath(PROGRAM_FILE) == @__FILE__)
+    thisfile = @__FILE__
+    error("The file '$thisfile' is not for direct execution")
+end 
+
+module plotting
+
+    # Import stuff
+    using Plots
+
+
+    function plot_pt(atmos, output_dir)
+        """
+        Plot the temperature-pressure profile.
+        """
+
+        println("Plotting PT profile")
+
+        arr_T = atmos.tmpl
+        arr_P = atmos.pl
+
+        plt = plot(arr_T, arr_P, 
+                legend=false, lc="black", lw=2)
+        xlabel!(plt, "Temperature [K]")
+        ylabel!(plt, "Pressure [Pa]")
+        yflip!(plt)
+        yaxis!(plt, yscale=:log10)
+                
+        savefig(joinpath(output_dir,"plot_pt.pdf"))
+
+    end
+
+    function plot_fluxes(atmos, output_dir)
+        """
+        Plot the fluxes at each pressure level
+        """
+
+        println("Plotting fluxes")
+
+        arr_P = atmos.pl
+
+        c = "red"
+        w = 2
+        plt = plot(-1.0.*atmos.flux_d_lw, arr_P, label="LW d", lw=w, lc=c, ls=:dot)
+        plot!(plt, atmos.flux_u_lw, arr_P, label="LW u", lw=w, lc=c, ls=:dash)
+        plot!(plt, atmos.flux_n_lw, arr_P, label="LW n", lw=w, lc=c, ls=:solid)
+
+        c = "blue"
+        plot!(plt, -1.0.*atmos.flux_d_sw, arr_P, label="SW d", lw=w, lc=c, ls=:dot)
+        plot!(plt, atmos.flux_u_sw, arr_P, label="SW u", lw=w, lc=c, ls=:dash)
+        plot!(plt, atmos.flux_n_sw, arr_P, label="SW n", lw=w, lc=c, ls=:solid)
+
+        xlabel!(plt, "Upward directed flux [W m-2]")
+        ylabel!(plt, "Pressure [Pa]")
+        xlabel!(plt, "Temperature [K]")
+        ylabel!(plt, "Pressure [Pa]")
+        yflip!(plt)
+        yaxis!(plt, yscale=:log10)
+
+        savefig(plt, joinpath(output_dir,"plot_fluxes.pdf"))
+
+    end
+    
+
+end
+
--- themis.jl
+++ themis.jl
@@ -8,6 +8,8 @@
 include("socrates/julia/src/SOCRATES.jl")
 include("src/radtrans.jl")
 include("src/solver.jl")
+include("src/plotting.jl")
+
 
 # Configuration options
 tstar           = 279.39    # LW uflux bottom boundary condition [kelvin]
@@ -17,24 +19,32 @@ gravity         = 9.81
 nlev_centre     = 69
 
 all_channels = true
-do_deallocate = true
 # spectral_file = "socrates/data/spectra/ga7/sp_lw_ga7" 
 spectral_file = "res/runtime_spectral_file_rscat"
 lw = true
+output_dir = "out/"
+
+# Create output direct
+rm(output_dir,force=true,recursive=true)
+mkdir(output_dir)
 
 # Allocate atmos object
 atmos = radtrans.Atmos_t()
 
-radtrans.setup_atmos!(atmos, spectral_file, nlev_centre,
+radtrans.setup_atmos!(atmos, spectral_file, all_channels,
                         false, false, false, false,
                         zenith_degrees, toa_heating, tstar,
-                        gravity
+                        gravity, nlev_centre
                         )
 
 radtrans.alloc_atmos!(atmos)
 
 radtrans.calc_fluxes!(atmos, true)
+radtrans.calc_fluxes!(atmos, false)
 
-display(atmos.radout.flux_down)
+
+plotting.plot_pt(atmos, output_dir)
+plotting.plot_fluxes(atmos, output_dir)
+
 
 println("Done!")
