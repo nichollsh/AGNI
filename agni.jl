f092578701a144d5baeed326fc84535296b64409
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2023-09-22 14:27:28 +0100

    Added min iters

--- agni.jl
+++ agni.jl
@@ -24,23 +24,21 @@ import phys
 
 
 # Configuration options
-tstar           = 1740.0    # LW uflux bottom boundary condition [kelvin]
-toa_heating     = 4.391e+04 # SW dflux top boundary condition [W m-2]
-radius          = 7.1e6     # metres
-gravity         = 10.0      # m s-2
+tstar           = 2500.0    # LW uflux bottom boundary condition [kelvin]
+toa_heating     = 1000.0 # SW dflux top boundary condition [W m-2]
+radius          = 6.0e6     # metres
+gravity         = 9.81      # m s-2
 nlev_centre     = 100  
-p_surf          = 280.4     # bar
+p_surf          = 270.0     # bar
 p_top           = 1e-6      # bar 
 mixing_ratios   = Dict([
-                        ("H2O" , 10.36251/280.4),
-                        ("CO2" , 94.02385/280.4),
-                        ("H2"  , 0.54123/280.4),
-                        ("CO"  , 172.91992/280.4),
-                        ("N2"  , 2.28491/280.4)
+                        ("H2O" , 1.0),
+                        ("CO2" , 0.0),
+                        ("CO"  , 0.0),
                         ])
 
-spfile_name   = "res/spectral_files/Mallard/Mallard"
-star_file     = "res/stellar_spectra/trappist-1.txt"
+spfile_name   = "res/spectral_files/Oak/Oak"
+star_file     = "res/stellar_spectra/sun.txt"
 output_dir    = "out/"
 
 # Create output directory
@@ -70,13 +68,13 @@ setpt.dry_adiabat!(atmos)
 setpt.stratosphere!(atmos, 300.0)
 
 # Calculate LW and SW fluxes (once)
-# println("RadTrans: calculating fluxes")
-atmosphere.radtrans!(atmos, true)
-atmosphere.radtrans!(atmos, false)
+#println("RadTrans: calculating fluxes")
+#atmosphere.radtrans!(atmos, true)
+#atmosphere.radtrans!(atmos, false)
 
 # Call solver 
 println("RadTrans: starting solver")
-# solver.solve_energy!(atmos, surf_state=2, modplot=1, verbose=true, dry_adjust=true, max_steps=500)
+solver.solve_energy!(atmos, surf_state=2, modplot=1, verbose=true, dry_adjust=true, h2o_adjust=true, max_steps=500, min_steps=40)
 
 # Write arrays
 atmosphere.write_ncdf(atmos,   joinpath(atmos.OUT_DIR,"atm.nc"))
