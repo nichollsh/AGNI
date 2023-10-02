bffc1eaa19f955f2749215d7bc3a31a433e41157
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2023-10-02 11:58:23 +0100

    More command-line options for PROTEUS integration

--- agni_cli.jl
+++ agni_cli.jl
@@ -17,7 +17,7 @@ trppt_default = 0.1
 s = ArgParseSettings()
 @add_arg_table s begin
     "tstar"
-        help = "LW uflux bottom boundary condition [K]."
+        help = "Surface temperature [K]."
         arg_type = Float64
         required = true
     "toa_heating"
@@ -52,6 +52,13 @@ s = ArgParseSettings()
         help = "Path to a CSV file containing a T(p) profile to load. Columns of file should be [Pa, K]"
         arg_type = String 
         default = ""
+    "--tstar_enforce"
+        help = "Do not allow the PT profile provided by `pt_path`` to overwrite the value of `t_star`"
+        action = :store_true
+    "--tmp_floor"
+        help = "Minimum temperature allowed in the model - prevents numerical issues [K]."
+        arg_type = Float64
+        default = 10.0
     "--ini_dry"
         help = "Initialise on a dry adiabat, with an isothermal stratosphere at this temperature."
         action = :store_true
@@ -63,9 +70,21 @@ s = ArgParseSettings()
         arg_type = Float64
         default = trppt_default
     "--surface"
-        help = "Surface state (0: free, 1: fixed at T(p_surf), 2: conductive skin)"
+        help = "Surface state (0: free, 1: fixed at T(p_surf), 2: conductive skin)."
         arg_type = Int
         default = 0
+    "--skin_d"
+        help = "Conductive skin thickness [m]."
+        arg_type = Float64
+        default = 0.1
+    "--skin_k"
+        help = "Conductive skin thermal conductivity [W m-1 K-1]."
+        arg_type = Float64
+        default = 2.0
+    "--tmp_magma"
+        help = "Magma temperature just below the surface [K]."
+        arg_type = Float64
+        default = 2000.0
     "--albedo_s"
         help = "Grey surface albedo."
         arg_type = Float64
@@ -152,6 +171,8 @@ x_path          = args["x_path"]
 oneshot         = args["once"]
 plot            = args["plot"]
 pt_path         = args["pt_path"]
+tstar_enforce   = args["tstar_enforce"]
+tmp_floor       = args["tmp_floor"]
 albedo_s        = args["albedo_s"]
 zenith_degrees  = args["zenith_degrees"]
 ini_dry         = args["ini_dry"]
@@ -162,6 +183,9 @@ rscatter        = args["rscatter"]
 verbose         = args["verbose"]
 animate         = args["animate"]
 surf_state      = args["surface"]
+skin_d          = args["skin_d"]
+skin_k          = args["skin_k"]
+tmp_magma       = args["tmp_magma"]
 max_steps       = args["nsteps"]
 no_adjust       = args["noadjust"]
 no_accel        = args["noaccel"]
@@ -232,6 +256,8 @@ atmosphere.setup!(atmos, ROOT_DIR, output_dir,
                          mf_path=mf_path,
                          zenith_degrees=zenith_degrees,
                          albedo_s=albedo_s,
+                         skin_d=skin_d, skin_k=skin_k, tmp_magma=tmp_magma,
+                         tmp_floor=tmp_floor,
                          flag_gcontinuum=true,
                          flag_rayleigh=rscatter
                          )
@@ -245,6 +271,10 @@ atmosphere.allocate!(atmos;
 if !(pt_path == "")
     setpt.fromcsv!(atmos, abspath(pt_path))
 end 
+#    Do not allow overwriting of tstar
+if tstar_enforce:
+    atmos.tstar = tstar 
+    atmos.tmpl[end] = star
 #    Prevent surface supersaturation
 setpt.prevent_surfsupersat!(atmos)
 #    Apply dry adiabat if required
