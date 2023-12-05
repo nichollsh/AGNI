7ec5db59bceeebd8b6c690f7294fad04a4a6371d
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2023-12-05 17:42:17 +0000

    Improvements to nlsolver. Options for different mixing length parameterisations.

--- src/solver_nlsol.jl
+++ src/solver_nlsol.jl
@@ -45,20 +45,12 @@ module solver_nlsol
     function solve_energy!(atmos::atmosphere.Atmos_t;
                             surf_state::Int=1, verbose::Bool=false,
                             dry_convect::Bool=true, sens_heat::Bool=false,
-                            max_steps::Int=500, atol::Float64=1.0e-4
+                            max_steps::Int=500, atol::Float64=1.0e-2
                             )
-
-        modprint = 10
-        call = 0
 
         # Objective function
         function fev!(F,x)
-
-            call += 1
            
-            # ----------------------------------------------------------
-            # Set atmosphere
-            # ----------------------------------------------------------
             # Read new guess
             for i in 1:atmos.nlev_c
                 atmos.tmp[i] = x[i]
@@ -75,21 +67,25 @@ module solver_nlsol
 
             # Interpolate temperature to cell-edge values 
             atmosphere.set_tmpl_from_tmp!(atmos, surf_state)
+
+            # Calculate layer properties 
+            atmosphere.calc_layer_props!(atmos)
             
+            # Reset fluxes
             atmos.flux_tot[:] .= 0.0
 
-            # Radiation
+            # +Radiation
             atmosphere.radtrans!(atmos, true)
             atmosphere.radtrans!(atmos, false)
             atmos.flux_tot += atmos.flux_n
 
-            # Convection
+            # +Dry convection
             if dry_convect
                 atmosphere.mlt!(atmos, pmin=100.0)
                 atmos.flux_tot += atmos.flux_c
             end
 
-            # Turbulence
+            # +Turbulence
             if sens_heat
                 atmosphere.sensible!(atmos)
                 atmos.flux_tot[end] += atmos.flux_sens
@@ -98,32 +94,12 @@ module solver_nlsol
             # Check fluxes are real numbers
             if !all(isfinite, atmos.flux_tot)
                 display(atmos.flux_tot)
-                error("Flux array contains NaNs")
+                error("Flux array contains NaNs and/or Infs")
             end
 
             # Pass outward
             for i in 1:atmos.nlev_c 
                 F[i] = atmos.flux_tot[i] - atmos.flux_tot[i+1]
-            end
-
-            if mod(call, modprint) == 0
-                F_OLR_rad = atmos.flux_u_lw[1]
-            
-                F_TOA_tot = atmos.flux_tot[1]
-                F_BOA_tot = atmos.flux_tot[end-1]
-                F_loss    = abs( F_TOA_tot-F_BOA_tot )
-
-                r_max = maximum(F) 
-                r_med = median(F)
-                
-                @printf("    call %d \n",  call )
-                @printf("    F_rad^OLR   = %+.2e W m-2  \n", F_OLR_rad)
-                @printf("    F_tot^TOA   = %+.2e W m-2  \n", F_TOA_tot)
-                @printf("    F_tot^BOA   = %+.2e W m-2  \n", F_BOA_tot)
-                @printf("    F_tot^loss  = %+.2e W m-2  \n", F_loss)
-                @printf("    r_max       = %+.2e W m-2  \n", r_max)
-                @printf("    r_med       = %+.2e W m-2  \n", r_med)
-                println(" ")
             end
 
             return nothing
@@ -136,14 +112,12 @@ module solver_nlsol
 
         println("NLSolver: begin Newton-Raphson-Backtracking iterations")
 
-
-        atmosphere.smooth_centres!(atmos, 5)
         atmosphere.set_tmpl_from_tmp!(atmos, surf_state)
 
         x0 = zeros(Float64, atmos.nlev_c)
         x0[:] .= atmos.tmp[:]
 
-        sol = nlsolve(fev!, x0, method = :newton, linesearch = BackTracking(), iterations=max_steps, ftol=atol)
+        sol = nlsolve(fev!, x0, method = :newton, linesearch = BackTracking(), iterations=max_steps, ftol=atol, show_trace=true)
 
         # ----------------------------------------------------------
         # Extract solution
@@ -164,11 +138,11 @@ module solver_nlsol
         loss = atmos.flux_tot[1] - atmos.flux_tot[end]
         loss_pct = loss/atmos.flux_tot[1]*100.0
         @printf("    endpoint fluxes \n")
-        @printf("    rad_OLR   = %.2e W m-2         \n", atmos.flux_u_lw[1])
-        @printf("    tot_TOA   = %.2e W m-2         \n", atmos.flux_tot[1])
-        @printf("    tot_BOA   = %.2e W m-2         \n", atmos.flux_tot[end])
-        @printf("    loss      = %.2e W m-2         \n", loss)
-        @printf("    loss      = %.2e %%            \n", loss_pct)
+        @printf("    rad_OLR   = %+.2e W m-2     \n", atmos.flux_u_lw[1])
+        @printf("    tot_TOA   = %+.2e W m-2     \n", atmos.flux_tot[1])
+        @printf("    tot_BOA   = %+.2e W m-2     \n", atmos.flux_tot[end])
+        @printf("    loss      = %+.2e W m-2     \n", loss)
+        @printf("    loss      = %+.2e %%        \n", loss_pct)
         @printf("\n")
 
     end # end solve_root
