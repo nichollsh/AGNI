39a3ef405c9b02870110a98d040d13e24a6d91c9
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-05-27 17:07:33 +0100

    Docs

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+push!(LOAD_PATH,"../src/")
+
+using Documenter, AGNI
+
+
+format = Documenter.HTML(edit_link = "main",
+                         prettyurls = get(ENV, "CI", nothing) == "true",
+                         assets = [
+                             joinpath("assets", "style.css"),
+                        ]
+)
+
+makedocs(
+    sitename="AGNI",
+    format=format,
+    pages = [
+        "Introduction"  => ["index.md"],
+        "Examples"      => ["examples.md"],
+        "Model"         => ["model.md"],
+        "Usage"         => ["usage.md"],
+        "Examples"      => ["examples.md"],
+        "API"           => ["api.md"]
+        # "Subsection" => [
+        #     ...
+        # ]
+    ]
+)
+
+
