fb88cdbd608dc81650d4c9cd49e56f7428625739
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-07-18 08:38:34 +0100

    Fixed to build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,25 @@
 # Build AGNI
 ROOT_DIR=abspath(joinpath(dirname(abspath(PROGRAM_FILE)),".."))
+println("ROOT_DIR = $ROOT_DIR")
 
 # Find socrates
 RAD_DIR = abspath(ENV["RAD_DIR"])
+println("RAD_DIR = $RAD_DIR")
 
 # Generate wrappers
+println("Generate wrappers")
 wrap = joinpath(RAD_DIR, "julia/src/generate_wrappers.jl")
 include(wrap)
 
 # Build libSOCRATES
+println("Build libSOCRATES")
 cd(joinpath(RAD_DIR,"julia/lib/")) do 
     run(`make`)
 end 
 
 # Download basic data 
-run(`bash $(joinpath(ROOT_DIR,get_data.sh)) basic`)
+println("Get data")
+get_data = joinpath(ROOT_DIR,"get_data.sh")
+run(`bash $get_data basic`)
 
+println("Build completed")
