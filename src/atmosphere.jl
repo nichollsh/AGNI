472974a2235430f3d94d24b0c9be8b57d087fe8f
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-12-03 11:51:00 +0000

    Fastchem max iter default

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -322,7 +322,7 @@ module atmosphere
                     use_all_gases::Bool =       false,
                     fastchem_work::String =     "",
                     fastchem_floor::Float64 =   273.0,
-                    fastchem_maxiter::Float64 = 240.0,
+                    fastchem_maxiter::Float64 = 2e4,
                     fastchem_xtol::Float64 =    1.0e-4,
                     )::Bool
 
