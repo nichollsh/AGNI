d08a80b772a0ca5e47b5030dd0a8c8922e37d0d8
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-07-20 11:16:21 +0100

    Version 1.7.0

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -26,7 +26,7 @@ module atmosphere
     import ..spectrum
 
     # Constants
-    const AGNI_VERSION::String   = "1.6.1"
+    const AGNI_VERSION::String   = "1.7.0"
     const HYDROGRAV_STEPS::Int64 = 40
 
     # Contains data pertaining to the atmosphere (fluxes, temperature, etc.)
