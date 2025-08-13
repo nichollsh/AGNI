d7bd90fc163abeeb40af0b41dd4235914c24d4dd
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-08-13 10:37:09 +0100

    Make topliq=_unset if not set by oc calc

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -754,7 +754,7 @@ module atmosphere
         atmos.ocean_cs_height = max(0.0, ocean_cs_height)
         atmos.ocean_maxdepth  = 0.0
         atmos.ocean_areacov   = 0.0
-        atmos.ocean_topliq    = "none"
+        atmos.ocean_topliq    = "_unset"
 
         # Set initial temperature profile to a small value which still keeps
         #   all of the gases supercritical. This should be a safe condition to
