a2288fe350c6546f51356e2e319844a1172f4c83
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-09-18 17:00:16 +0100

    Save profiles from grid

--- misc/grid_agni.jl
+++ misc/grid_agni.jl
@@ -2,6 +2,7 @@ using AGNI
 using .Iterators
 using LoggingExtras
 using Printf
+using NCDatasets
 
 const ROOT_DIR::String = abspath(dirname(abspath(@__FILE__)), "../")
 const R_earth::Float64 = 6.371e6
@@ -12,23 +13,24 @@ const M_earth::Float64 = 5.972e24
 # -------------------------------
 
 # Base parameters
-cfg_base = "res/config/l9859d.toml"
+cfg_base = "res/config/greygas.toml"
 @info "Using base config: $cfg_base"
 cfg::Dict = AGNI.open_config(joinpath(ROOT_DIR,cfg_base))
 
 # Define grid
 grid::Dict = Dict((
-    # "mass" => range(start=1.57*M_earth,   stop=2.39*M_earth,  length=2),
-    "vmr_H2S" => range(start=0.02,        stop=0.5,           length=8),
-    "p_surf"  => 10.0 .^ range(start=1.5, stop=4.5,           length=7),
+    "mass_tot"  => range(start=1.00,  stop=6.00,  length=2),  # M_earth
+    "frac_atm"  => range(start=0.01,  stop=0.10,  length=2),
+    "frac_core" => range(start=0.10,  stop=0.80,  length=2),
 ))
 
 # Variables to record
-output_keys = ["p_boa", "tmp_surf", "transspec_r", "transspec_rho", "transspec_μ", "transspec_tmp", "mmw_surf", "Kzz_kbreak"]
+output_keys = ["p_surf", "t_surf", "r_surf", "μ_surf", "r_phot", "μ_phot", "t_phot",  "Kzz_max"]
 
 # Other options
-save_netcdfs = false
-save_plots   = false
+save_netcdfs = false        # NetCDF file for each case
+save_plots   = false        # plots for each case
+save_ncdf_tp = true         # a NetCDF containing all T(p) solutions
 
 # =============================================================================
 # Parse keys and flatten grid
@@ -66,10 +68,15 @@ p_surf           = cfg["composition"]["p_surf"]
 mf_dict          = cfg["composition"]["vmr_dict"]
 star_Teff        = cfg["planet"]["star_Teff"]
 stellar_spectrum = cfg["files"]["input_star"]
+nlev_c           = cfg["execution"]["num_levels"]
 
-radius  = cfg["planet"]["radius"]
-mass    = cfg["planet"]["mass"]
-gravity = phys.grav_accel(mass, radius)
+# Intial values for interior structure
+radius   = cfg["planet"]["radius"]
+mass     = cfg["planet"]["mass"] # interior mass
+gravity  = phys.grav_accel(mass, radius)
+mass_tot  = mass * 1.1
+frac_core = 0.325
+frac_atm  = 0.01
 
 # Get the keys from the grid dictionary
 input_keys = collect(keys(grid))
@@ -94,8 +101,9 @@ open(joinpath(output_dir,"gridpoints.csv"), "w") do hdl
     end
 end
 
-# Create dictionary to record results in
-result::Array{Dict, 1} = [Dict{String,Float64}() for _ in 1:gridsize]
+# Create output variables to record results in
+result_table::Array{Dict, 1}  = [Dict{String,Float64}() for _ in 1:gridsize]
+result_profs::Array{Dict, 1}  = [Dict{String,Array}()    for _ in 1:gridsize] # array of dicts (p, t, r)
 
 @info "Generated grid of $(length(input_keys)) dimensions, with $gridsize points"
 
@@ -115,7 +123,7 @@ atmosphere.setup!(atmos, ROOT_DIR, output_dir,
                                 Float64(cfg["planet"]["zenith_angle"]),
                                 Float64(cfg["planet"]["tmp_surf"]),
                                 gravity, radius,
-                                Int(cfg["execution"]["num_levels"]),
+                                nlev_c,
                                 p_surf,
                                 p_top,
                                 mf_dict, "";
@@ -129,6 +137,7 @@ atmosphere.setup!(atmos, ROOT_DIR, output_dir,
                                 thermo_functions  = cfg["execution"]["thermo_funct"],
                                 use_all_gases     = true,
                                 C_d=turb_coeff, U=wind_speed,
+                                Kzz_floor = 0.0,
                                 flux_int=flux_int,
                                 surface_material=surface_mat,
                                 mlt_criterion=only(cfg["execution"]["convection_crit"][1]),
@@ -143,6 +152,50 @@ setpt.request!(atmos, cfg["execution"]["initial_state"])
 # =============================================================================
 # Iterate over parameters
 # -------------------------------
+
+"""
+Calc interior radius as a function of: interior mass, interior core mass fraction.
+    Earth units.
+"""
+function calc_Rint(m, c)
+    # fit coefficients
+    m0 = 1.10190624
+    m1 = 0.27968094
+    c0 = -0.20955757
+    c1 = 2.07892259
+    e0 = -0.08667749
+    e1 = 0.66494823
+    o1 = -0.0593633
+
+    # evaluate fit
+    return  m0*m^m1 + c0*c^c1 + e0*(m*c)^e1  + o1
+end
+
+"""
+Update interior structure
+"""
+function update_structure!(atmos, mass_tot, frac_atm, frac_core)
+    if frac_atm + frac_core > 1
+        @warn "Core fraction ($frac_core) and atm fraction ($frac_atm) sum to >1"
+    end
+
+    # atmosphere mass
+    mass_atm = mass_tot * frac_atm
+
+    # interior mass from remainder
+    atmos.interior_mass = mass_tot - mass_atm
+
+    # get interior radius from fit to Zalmoxis
+    frac_core_int = frac_core / (1-frac_atm)
+    atmos.rp = calc_Rint(atmos.interior_mass/M_earth, frac_core_int) * R_earth
+
+    # surface gravity
+    atmos.grav_surf = phys.grav_accel(atmos.interior_mass, atmos.rp)
+
+    # surface pressure
+    atmos.p_boa = mass_atm * atmos.grav_surf / (4 * pi * atmos.rp^2)
+    atmosphere.generate_pgrid!(atmos)
+end
 
 for (i,p) in enumerate(grid_flat)
     @info @sprintf("Grid point %d / %-d = %.1f%%",i,gridsize,100*i/gridsize)
@@ -163,12 +216,24 @@ for (i,p) in enumerate(grid_flat)
             atmosphere.generate_pgrid!(atmos)
 
         elseif key == "mass"
-            atmos.interior_mass = val
-            atmos.grav_surf = phys.grav_accel(val, radius)
+            atmos.interior_mass = val * M_earth
+            atmos.grav_surf = phys.grav_accel(val, atmos.rp)
 
         elseif key == "radius"
             atmos.rp = val
-            atmos.grav_surf = phys.grav_accel(atmos.interior_mass, val)
+            atmos.grav_surf = phys.grav_accel(atmos.interior_mass, atmos.rp)
+
+        elseif key == "frac_atm"
+            global frac_atm = val
+            update_structure!(atmos, mass_tot, frac_atm, frac_core)
+
+        elseif key == "frac_core"
+            global frac_core = val
+            update_structure!(atmos, mass_tot, frac_atm, frac_core)
+
+        elseif key == "mass_tot"
+            global mass_tot = val * M_earth
+            update_structure!(atmos, mass_tot, frac_atm, frac_core)
 
         elseif key == "instellation"
             atmos.instellation = val
@@ -207,9 +272,9 @@ for (i,p) in enumerate(grid_flat)
     # set original vmr arrays
     for g in atmos.gas_names
         atmos.gas_ovmr[g][:] .= atmos.gas_vmr[g][:]
-        if atmos.gas_vmr[g][end] > 0
-            println("$g: $(atmos.gas_vmr[g][end])")
-        end
+        # if atmos.gas_vmr[g][end] > 0
+        #     println("$g: $(atmos.gas_vmr[g][end])")
+        # end
     end
 
     # Solve for RCE
@@ -231,6 +296,10 @@ for (i,p) in enumerate(grid_flat)
                                             perturb_all=perturb_all
                                             )
 
+    println("Rp     = $(atmos.rp/R_earth)  R_earth")
+    println("r_end  = $(atmos.r[end]/R_earth) R_earth")
+    println("rl_end = $(atmos.rl[end]/R_earth) R_earth")
+
     # Write NetCDF file
     if save_netcdfs
         save.write_ncdf(atmos, joinpath(atmos.OUT_DIR,@sprintf("%07d.nc",i)))
@@ -245,14 +314,34 @@ for (i,p) in enumerate(grid_flat)
     for k in output_keys
         field = Symbol(k)
         if hasfield(atmosphere.Atmos_t, field)
-            result[i][k] = Float64(getfield(atmos, Symbol(k)))
-        elseif k == "mmw_surf"
-            result[i][k] = Float64(atmos.layer_μ[end])
+            result_table[i][k] = Float64(getfield(atmos, Symbol(k)))
+        elseif k == "p_surf"
+            result_table[i][k] = atmos.p_boa
+        elseif k == "t_surf"
+            result_table[i][k] = atmos.tmp_surf
+        elseif k == "r_surf"
+            result_table[i][k] = atmos.rp
+        elseif k == "μ_surf"
+            result_table[i][k] = atmos.layer_μ[end]
+        elseif k == "r_phot"
+            result_table[i][k] = atmos.transspec_r
+        elseif k == "μ_phot"
+            result_table[i][k] = atmos.transspec_μ
+        elseif k == "t_phot"
+            result_table[i][k] = atmos.transspec_tmp
+        elseif k == "Kzz_max"
+            result_table[i][k] = maximum(atmos.Kzz)
         else
             @error "Unhandled variable: $k"
-            result[i][k] = 0.0
+            result_table[i][k]  = 0.0
         end
     end
+
+    # Record profile (also in SI)
+    result_profs[i] = Dict("p"=>deepcopy(atmos.p),
+                            "t"=>deepcopy(atmos.tmp),
+                            "r"=>deepcopy(atmos.r)
+                        )
 
     # Iterate
     @info "  "
@@ -261,12 +350,12 @@ end
 atmosphere.deallocate!(atmos)
 
 # =============================================================================
-# Write result to file, and exit
+# Write result_table to CSV, result_profs to NetCDF, and exit
 # -------------------------------
 
 # Write results
-@info "Writing results to file..."
-open(joinpath(output_dir,"result.csv"), "w") do hdl
+@info "Writing result_table to CSV..."
+open(joinpath(output_dir,"result_table.csv"), "w") do hdl
     # Header
     head = "index," * join(output_keys, ",") * "\n"
     write(hdl,head)
@@ -275,11 +364,38 @@ open(joinpath(output_dir,"result.csv"), "w") do hdl
     for i in 1:gridsize
         row = @sprintf("%07d",i)
         for k in output_keys
-            row *= @sprintf(",%.6e",result[i][k])
+            row *= @sprintf(",%.6e",result_table[i][k])
         end
         write(hdl,row*"\n")
     end
 end
+
+# Write NetCDF of profiles
+@info "Writing result_profs to NetCDF.."
+ds = Dataset(joinpath(output_dir,"result_profs.nc"),"c")
+ds.attrib["description"]        = "AGNI grid profiles (TPR)"
+ds.attrib["hostname"]           = gethostname()
+ds.attrib["username"]           = ENV["USER"]
+ds.attrib["AGNI_version"]       = atmos.AGNI_VERSION
+ds.attrib["SOCRATES_version"]   = atmos.SOCRATES_VERSION
+
+defDim(ds, "nlev_c",   atmos.nlev_c)
+defDim(ds, "gridsize", gridsize)
+
+var_p = defVar(ds, "p", Float64, ("nlev_c","gridsize",) ) # saved in python dimension order
+var_t = defVar(ds, "t", Float64, ("nlev_c","gridsize",) )
+var_r = defVar(ds, "r", Float64, ("nlev_c","gridsize",) )
+
+for i in 1:gridsize
+    for j in 1:nlev_c
+        var_p[j,i] = result_profs[i]["p"][j]
+        var_t[j,i] = result_profs[i]["t"][j]
+        var_r[j,i] = result_profs[i]["r"][j]
+    end
+    @info @sprintf("%d : Ri=%.3f",i,result_profs[i]["r"][end]/R_earth)
+end
+
+close(ds)
 
 # Done
 @info "Done!"
