79f574e0e65308f66f14b2fe112e6d48bb0a1642
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-11-18 10:47:12 +0000

    Fix plot label spam

--- src/plotting.jl
+++ src/plotting.jl
@@ -53,10 +53,10 @@ module plotting
 
     # Reusable plotting snippets
     macro _plt_pboa()
-        return esc(:(hline!(plt, [atmos.p_boa/1e5 ], color="black",  ls=:solid)))
+        return esc(:(hline!(plt, [atmos.p_boa/1e5 ], label="", color="black",  ls=:solid)))
     end
     macro _plt_poboa()
-        return esc(:(hline!(plt, [atmos.p_oboa/1e5], color="black", ls=:dot)))
+        return esc(:(hline!(plt, [atmos.p_oboa/1e5], label="", color="black", ls=:dot)))
     end
 
 
