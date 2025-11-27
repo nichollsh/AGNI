0cf8bf8600f9ed50aaf8ab5a220a70f8191ed3b1
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-11-27 14:56:04 +0100

    Fix bug when setting Teff and nwork>1

--- misc/grid/slurm.sh
+++ misc/grid/slurm.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-#SBATCH --time=03:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --time=00-03:00:00
+#SBATCH --cpus-per-task=10
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=3G
 #SBATCH --output=slurm-grid-%j.log
 
-echo "Workers: $SLURM_CPUS_PER_TASK"
-echo "Time: $SBATCH_TIMELIMIT"
-echo "Node: $SLURM_NODEID"
+echo "Allocated workers: $SLURM_CPUS_PER_TASK"
+echo "Allocated node:    $SLURM_NODEID"
 
+echo "Start time:        $(date -d @$SLURM_JOB_START_TIME)"
+echo "Start time:        $(date -d @$SLURM_JOB_END_TIME)"
+
+echo " "
 julia -t$SLURM_CPUS_PER_TASK misc/grid/manager.jl
+echo " "
 
 echo "Done"
