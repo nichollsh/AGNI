06254ac2034ad76632391421463a0e561d1bc8a1
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-12-08 09:22:29 +0000

    Working fairly well. Reset tmp_surf for next gridpoint if current one failed

--- misc/grid/slurm.sh
+++ misc/grid/slurm.sh
@@ -2,8 +2,8 @@
 
 # CHANGE
 #SBATCH --time=00-24:00:00
-#SBATCH --cpus-per-task=30
-#SBATCH --mem-per-cpu=2700M
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=2400M
 
 # LEAVE
 #SBATCH --export=ALL
